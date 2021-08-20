38b10a6056eb4381f561e8a32235e427877ff78b
hskuza <hubert.skuza2011@gmail.com>
2021-08-20 09:03:03 +0200

    blog data

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,25 @@
 import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-	{
-		slug: "getting-started-with-nextjs1",
-		title: "Getting started with nextJS",
-		image: "getting-started-nextjs.png",
-		excerpt: "NextJS is a the React framework for production",
-		date: "2022-02-10",
-	},
-	{
-		slug: "getting-started-with-nextjs2",
-		title: "Getting started with nextJS",
-		image: "getting-started-nextjs.png",
-		excerpt: "NextJS is a the React framework for production",
-		date: "2022-02-10",
-	},
-	{
-		slug: "getting-started-with-nextjs3",
-		title: "Getting started with nextJS",
-		image: "getting-started-nextjs.png",
-		excerpt: "NextJS is a the React framework for production",
-		date: "2022-02-10",
-	},
-	{
-		slug: "getting-started-with-nextjs4",
-		title: "Getting started with nextJS",
-		image: "getting-started-nextjs.png",
-		excerpt: "NextJS is a the React framework for production",
-		date: "2022-02-10",
-	},
-];
-
-function HomePage() {
+function HomePage(props) {
 	return (
 		<Fragment>
 			<Hero />
-			<FeaturedPosts posts={DUMMY_POSTS} />
+			<FeaturedPosts posts={props.posts} />
 		</Fragment>
 	);
+}
+
+export function getStaticProps() {
+	const featuredPosts = getFeaturedPosts();
+
+	return {
+		props: {
+			posts: featuredPosts,
+		},
+	};
 }
 
 export default HomePage;
